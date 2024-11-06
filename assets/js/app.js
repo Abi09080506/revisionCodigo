2b0e9f8dced004aed62dd9b38da22bee2aece55e
Abi <pme05592@gmail.com>
2024-11-05 22:02:19 -0600

    Se logró mostrar estilos, y no hubo errores. Los datos del usuario aparecen en la página

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,28 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
+
+const $n = document.querySelector('.name');// se aggregó una clase válida al agregar un .
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
-
-function displayUser(username) {
+// Por el uso de un await, asumo que busca ejecutar una función asincrona; por lo que se hará un cambio en la estructura con la finalidad de que se cumpla el objetivo, es decir que indica la carga
+async function displayUser(username) {
   $n.textContent = 'cargando...';
+  try{
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+  const data=await response.json(); // convierte la respuesta en un objeto para manipular los datos como un objeto
 
+  $n.textContent = `${data.name}`; //cambio de comillas para que tome la variable y no el texto como en cargando
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
+} catch (error) {
+  handleError(error);
+}
+}
+//
 function handleError(err) {
   console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  console.log(err); 
+  n.textContent = 'Algo salió mal: ' + `${err}`; //cambio por comillas simples y agregar ;, también cambio de para que se agregué el mensaje y la variable
 }
 
 displayUser('stolinski').catch(handleError);
